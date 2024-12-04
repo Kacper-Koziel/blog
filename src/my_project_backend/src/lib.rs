068a1362cb03d3cb666711e034b2deb1e8618afc
Kacper-Koziel - <kacperiusz2018@gmail.com>
2024-12-04 15:50:13 +0000

    Updated frontend

--- src/my_project_backend/src/lib.rs
+++ src/my_project_backend/src/lib.rs
@@ -14,7 +14,7 @@ fn add_blog(title: String, content: String, tags: Vec<String>) -> Result<Blog, S
         return Err("Title is too long".to_string())
     }
 
-    if content.len() > 500
+    if content.len() > 2000
     {
         return Err("Content is too long".to_string())
     }
